dc0fd6c0bdc25e85d21753f0b10b61f3c550de94
Martin Mungai <mungaimrtn@gmail.com>
2023-11-30 17:43:54 +0300

    initial commit

---
+++ iACC/UIViewController+Navigation.swift
@@ -0,0 +1,24 @@
+//	
+// Copyright © Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIViewController {
+    @objc func addCard() {
+        show(AddCardViewController(), sender: self)
+    }
+    
+    @objc func addFriend() {
+        show(AddFriendViewController(), sender: self)
+    }
+    
+    @objc func sendMoney() {
+        show(SendMoneyViewController(), sender: self)
+    }
+    
+    @objc func requestMoney() {
+        show(RequestMoneyViewController(), sender: self)
+    }
+}
